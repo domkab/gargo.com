eff02d5de5df051a78c77af65e34cb4cc4420bb0
domkab <dominykas.bobinas@gmail.com>
2024-01-09 16:38:04 +0200

    1.0.5 added particles

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,116 @@
 /* eslint-disable no-console */
-// import { Routes } from 'react-router-dom';
 import './App.scss';
 import { About } from './Components/About';
 import { Header } from './Components/Header';
 import { Hero } from './Components/Hero';
 import { Works } from './Components/Works';
 
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+} from "@tsparticles/engine";
+
+import { loadSlim } from "@tsparticles/slim";
+
 const App: React.FC = () => {
   console.log('rendering app');
-  
+
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async engine => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "transparent",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          random: false,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 60,
+        },
+        opacity: {
+          value: 0.3,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
   return (
     <div className="App">
+      {init && (
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+        />
+      )}
       <Header />
       <Hero />
       <Works />
       <About />
-      {/* <Routes>
-      </Routes> */}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -24,3 +24,76 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 80,
+//       density: {
+//         enable: true,
+//         value_area: 800
+//       }
+//     },
+//     shape: {
+//       type: 'circle',
+//     },
+//     opacity: {
+//       value: 0.4,
+//       random: false,
+//       anim: {
+//         enable: false,
+//       }
+//     },
+//     size: {
+//       value: 6,
+//       random: true,
+//       anim: {
+//         enable: false,
+//       }
+//     },
+//     line_linked: {
+//       enable: false,
+//     },
+//     move: {
+//       enable: true,
+//       speed: 2,
+//       direction: 'none',
+//       random: false,
+//       straight: false,
+//       out_mode: 'out',
+//       bounce: false,
+//       attract: {
+//         enable: false,
+//         rotateX: 600,
+//         rotateY: 1200
+//       }
+//     }
+//   },
+//   interactivity: {
+//     detect_on: 'canvas',
+//     events: {
+//       onhover: {
+//         enable: true,
+//         mode: 'bubble'
+//       },
+//       onclick: {
+//         enable: true,
+//         mode: 'repulse'
+//       },
+//       resize: true
+//     },
+//     modes: {
+//       bubble: {
+//         distance: 400,
+//         size: 40,
+//         duration: 2,
+//         opacity: 8,
+//         speed: 3
+//       },
+//       repulse: {
+//         distance: 200,
+//         duration: 0.4
+//       }
+//     }
+//   },
+//   retina_detect: true
+// };
